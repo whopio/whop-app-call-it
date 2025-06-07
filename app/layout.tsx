17fb8d726a9cf2bfc5a8864c8557c619985dae2d
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-06-06 17:26:56 -0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { WhopIframeSdkProvider, WhopThemeScript } from "@whop/react";
+import { Theme } from "@whop/react/components";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Whop App",
+	description: "My Whop App",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<head>
+				<WhopThemeScript />
+			</head>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<Theme accentColor="blue">
+					<WhopIframeSdkProvider>{children}</WhopIframeSdkProvider>
+				</Theme>
+			</body>
+		</html>
+	);
+}
